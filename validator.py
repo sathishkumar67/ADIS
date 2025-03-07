70935412145ea5cddaf186c81a24aa2f7ec60713
sathishkumar67 <sathishvillers2000@gmail.com>
2025-03-07 09:46:42 +0530

    updated

--- validator.py
+++ validator.py
@@ -93,7 +93,7 @@ class DetectionValidator(BaseValidator):
 
     def get_desc(self):
         """Return a formatted string summarizing class metrics of YOLO model."""
-        return ("%22s" + "%11s" * 6) % ("Class", "Images", "Instances", "Box(P", "R", "mAP50", "mAP50-95)", "IOU", "Accuracy")
+        return ("%22s" + "%11s" * 6) % ("Class", "Images", "Instances", "Box(P", "R", "mAP50", "mAP50-95)")
 
     def postprocess(self, preds):
         """Apply Non-maximum suppression to prediction outputs."""
