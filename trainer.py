001130a125e1708d2d782ded3a52ad16f944d473
sathishkumar67 <sathishvillers2000@gmail.com>
2025-04-07 14:36:19 +0530

    used fused optimizer and modified ssd implementation

--- trainer.py
+++ trainer.py
@@ -850,7 +850,7 @@ class DetectionTrainer:
             LOGGER.info("Closing dataloader mosaic")
             self.train_loader.dataset.close_mosaic(hyp=copy(self.args))
 
-    def build_optimizer(self, model, name="auto", lr=0.001, momentum=0.9, decay=1e-5):
+    def build_optimizer(self, model, name="auto", lr=0.0001, momentum=0.9, decay=1e-5):
         """
         Constructs an optimizer for the given model, based on the specified optimizer name, learning rate, momentum,
         weight decay, and number of iterations.
@@ -879,7 +879,7 @@ class DetectionTrainer:
                 else:  # weight (with decay)
                     g[0].append(param)
                     
-        optimizer = optim.AdamW(g[2], lr=lr, betas=(momentum, 0.999), weight_decay=0.0)
+        optimizer = optim.AdamW(g[2], lr=lr, betas=(momentum, 0.999), weight_decay=0.0, fused=True)
 
         optimizer.add_param_group({"params": g[0], "weight_decay": decay})  # add g0 with weight_decay
         optimizer.add_param_group({"params": g[1], "weight_decay": 0.0})  # add g1 (BatchNorm2d weights)
